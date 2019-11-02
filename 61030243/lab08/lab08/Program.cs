1ab3fbc77a8e6d945f3f2eb4df41314d2549ed8b
WidsawaD <safefire@gmail.com>
2019-11-02 14:35:05 +0700

    exercise 1

--- 61030243/lab08/lab08/Program.cs
+++ 61030243/lab08/lab08/Program.cs
@@ -10,26 +10,23 @@ namespace lab08
     {
         static void Main(string[] args)
         {
-            string[] array1 = { "cat", "dog", "carrot", "bird" };
-            //
-            // ค้นหาสมาชิกตัวแรกที่มีค่าตามกำหนด
-            //
-            string value1 = Array.Find(array1, element => element.StartsWith("car", StringComparison.Ordinal));
+            string[] month = {
+                "January",
+                "February",
+                "March",
+                "Aprill",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "November",
+                "December",
+            };
 
-            string value2 = Array.Find(array1, element => element.StartsWith("fish", StringComparison.Ordinal));
-
-            //
-            // ค้นหาสมาชิกตัวแรกที่มีความยาว string ตามกำหนด
-            //
-            string value3 = Array.Find(array1, element => element.Length == 3);
-            //
-            // ค้นหา string ที่มีความยาวไม่เกินค่าที่กำหนด
-            //
-            string[] array2 = Array.FindAll(array1, element => element.Length <= 4);
-            Console.WriteLine(value1);
-            Console.WriteLine(value2);
-            Console.WriteLine(value3);
-            Console.WriteLine(string.Join(",", array2));
+            Console.WriteLine("little size : " + Array.Find(month, element => element.Length == 3));
+            Console.WriteLine("Long than 10 : " + Array.Find(month, element => element.Length > 10));
+            Console.WriteLine("start of J : " + string.Join(",", Array.FindAll(month, element => element.StartsWith("J", StringComparison.Ordinal))));
             Console.ReadLine();
         }
     }
